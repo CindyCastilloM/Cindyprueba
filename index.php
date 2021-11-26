51742988d7fd5ab7ca0c1dcd7173ac659ba3f45b
CindyCastilloM <95061133+CindyCastilloM@users.noreply.github.com>
2021-11-25 20:18:19 -0600

    Primer commit

---
+++ index.php
@@ -0,0 +1,145 @@
+<html lang="es">
+<head>
+  <!-- FAVICON-->
+<link rel="icon" type="image/png" href="i3.png" sizes="16x16">
+<style>
+
+
+body {
+  font-size: 28px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+}
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #CC2EFA;
+}
+.active {
+  background-color: #CC2EFA;
+}
+div {
+  padding: 70px;
+  border: 1px solid #CC2EFA;
+  font-size: 36px;
+  background-color: #E6E6E6;
+color: #8904B1;
+font-family: 'Open Sans';
+}
+
+
+
+
+
+
+</style>
+
+
+
+
+
+<marquee bgcolor="BLACK" behavior="scroll" direction="left">
+
+<b><font face="MV Boli" color="6633FF" size="7">Smart Education</font></b>
+</marquee>
+
+
+<ul>
+  <li><a class="active" href="index.php">Home</a></li>
+
+  <li><a href="video.php">Videos</a></li>
+  
+  <li><a href="lecciones.php">Lecciones</a></li>
+  <li><a href="cuentos.php">Cuentos</a></li>
+  <li><a href="vip.html">VIP</a></li>
+  <li><a href="contacto.php">Sobre nosotros</a></li>
+
+</ul>
+
+
+<body background="f2.png">
+
+<!-- https://www.curso-ingles.com/-->
+
+<!-- TITULO PAGINA -->
+<title> Smart Education </title>
+</head>
+<body>
+
+
+
+
+<center>
+<div><B><font  face="Arial" size="6" color="black"> Aprende inglés de la mejor manera</B><br></font>
+
+<font  face="Open Sans" size="6" >Adquiere el aprendizaje que necesitas de manera fácil y rápida
+</div>
+<img  align="center" src="i1.jpg">
+
+
+</center>
+<br><br>
+
+<!-- TABLA -->
+<table border="0" bgcolor="white" align="center" width="1000" >
+
+ <th><br><img src="aprende.png"><font  face="Arial" size="6" color="black"><br>Aprende lo que necesitas 
+  <font  face="Open Sans" size="5" color="black"> <br>Gracias a los cursos especializados que tenemos para ti</Th>
+
+  <th><br><img src="mejora.png"><font  face="Arial" size="6" color="black"><br>Prepárate para una carrera 
+    <font  face="Open Sans" size="5" color="black"> <br>Para así poder obtener mejores empleos en el futuro</Th>
+
+    <th><br><br><img src="obten.png"><font  face="Arial" size="6" color="black"><br>Obtén un certificado o título 
+      <font  face="Open Sans" size="5" color="black"> <br>Y así poder tener papeles que avalen tu desempeño</Th>
+
+      <th><br><img src="aprende.png"><font  face="Arial" size="6" color="black"><br>Mejora tu comunicación 
+        <font  face="Open Sans" size="5" color="black"> <br>Con videos que ayuden a mejorar tu habla en el inglés</Th>
+
+   </tr>
+
+<!--
+body { cursor: crosshair}
+-->
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+<!-- https://www.curso-ingles.com/-->
+
+
+
+
+
+
+
+
+
